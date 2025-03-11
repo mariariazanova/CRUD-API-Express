f600b21799f55f95a703b0d7462bdd3a605c8235
Maria Riazanova <57406080+mariariazanova@users.noreply.github.com>
2025-03-12 00:29:02 +0100

    Delete dist directory

--- dist/constants/urls.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.USERS_ID_URL = exports.USERS_URL = void 0;
-exports.USERS_URL = '/users';
-exports.USERS_ID_URL = `${exports.USERS_URL}/:id`;
-//# sourceMappingURL=urls.js.map
--- dist/utils/sendJsonResponse.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendJsonResponse = void 0;
-const sendJsonResponse = (res, value, statusCode = 200, message = '') => {
-    res.status(statusCode).json(value || { error: message });
-};
-exports.sendJsonResponse = sendJsonResponse;
-//# sourceMappingURL=sendJsonResponse.js.map
